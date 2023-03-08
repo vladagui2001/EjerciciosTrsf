8623d319dfb137e2b636d269498332a87c65a3e3
VladAgui2001 <vladagui2001@corp.amazonworkspaces.com>
2023-03-07 21:21:15 -0600

    Ejercicios de la sesión 7

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Primer ejercicio 
+Console.WriteLine("\nEjercicio 1");
+
+int n1 = 30, n2 = 20;
+if (n1 > n2)
+{
+    int dif = n1 - n2, dob = dif*2;
+
+    Console.WriteLine( "La diferencia entre {0} y {1} es de {2}", n1, n2, dif, dob);
+    Console.WriteLine( "Y su doble sería {0}", dob);
+}
+else
+{
+    Console.WriteLine("El segundo número es mayor que el primero");
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Segundo Ejercicio
+
+Console.WriteLine("\nEjercicio 2");
+
+int i;
+ for(i=1; i<=99; i++)
+    {
+        if(i%2!=0)
+        {
+            Console.WriteLine( "{0}", i);
+        }
+    }
+

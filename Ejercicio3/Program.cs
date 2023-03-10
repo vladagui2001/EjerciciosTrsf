ac55e66b41bad1b5f9920764ede550e85ada2cd5
VladAgui2001 <vladagui2001@corp.amazonworkspaces.com>
2023-03-09 19:49:36 -0600

    Ejercicio Sesión 8

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ejercicio 1 Sesión 8");
+
+double a, b, p, ar, c;
+
+Console.Write("Escriba el primer valor: ");
+a = double.Parse(Console.ReadLine());
+
+Console.Write("Escriba el segundo valor: ");
+b = double.Parse(Console.ReadLine());
+
+p = (a*2) + (b*2);
+Console.WriteLine("El perímetro es: {0}",p);
+
+ar = a * b; 
+Console.WriteLine("El área es: {0}",ar);
+
+c = Math.Sqrt(a * a + b * b);
+Console.WriteLine("La Diagonal es: {0}",c);

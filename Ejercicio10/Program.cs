ff188c53f86d541ce829178d1096e8ba23ae9e86
VladAgui2001 <vladagui2001@corp.amazonworkspaces.com>
2023-03-26 20:36:00 -0600

    Sesión 12 Ejercicio 1

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> stringList = new List<string> {"Piedras", "Agua", "Tierra", "Tormenta", "Fuego"};
+
+        Console.WriteLine($"El numero de palabras con la letra 'a' es de {ContadorCaracteres(stringList, 'a')} caracteres");
+    }
+
+    static int ContadorCaracteres(List<string> strings, char letra)
+    {
+        int i = 0;
+
+        foreach (string str in strings)
+        {
+            if (str.Contains(letra))
+            {
+                i++;
+            }
+        }
+
+        return i;
+    }
+}

6847229fc91dbf273f40fa8e0f849df9a6838f40
VladAgui2001 <vladagui2001@corp.amazonworkspaces.com>
2023-03-26 21:39:13 -0600

    Sesión 12 Ejercicio 2

--- Ejercicio11/Program.cs
+++ Ejercicio11/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        double radio = 5.0; 
+        double cir = CalculoCircumf(radio);
+        Console.WriteLine($"La circumferencia de un circulo con radio: {radio} es de {cir}");
+    }
+
+    static double CalculoCircumf(double radio)
+    {
+        double cir = 2 * Math.PI * radio;
+        return cir;
+    }
+}

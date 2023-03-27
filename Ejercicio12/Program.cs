b92f9f2375de8ea9dd6c5f15d1dea801c24b6640
VladAgui2001 <vladagui2001@corp.amazonworkspaces.com>
2023-03-26 22:41:33 -0600

    Sesión 12 Ejercicio 3

---
+++ Ejercicio12/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Example usage:
+        Console.Write("Ingresa un texto con espacios: ");
+        string input = Console.ReadLine();
+        int ContEsp = EspCont(input);
+        Console.WriteLine($"The input string contains {ContEsp} spaces.");
+    }
+
+    static int EspCont(string input)
+    {
+        int ContEsp = 0;
+        foreach (char letra in input)
+        {
+            if (letra == ' ')
+            {
+                ContEsp++;
+            }
+        }
+        return ContEsp;
+    }
+}

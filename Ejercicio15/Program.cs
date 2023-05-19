06252c8e8be1e1f0b2422f220120b176daed8cd8
VladAgui2001 <vlad.aguiluz5c@gmail.com>
2023-05-19 10:32:23 -0600

    Sesión 17 Ejercicio 2 	new file:   Ejercicio15/Ejercicio15.csproj 	new file:   Ejercicio15/Program.cs

---
+++ Ejercicio15/Program.cs
@@ -0,0 +1,18 @@
+﻿var students = new Dictionary<string, string>();
+
+students.Add("2323", "Eduardo López");
+students.Add("3282", "Diego Montez");
+students.Add("9012", "Vlad Enrique");
+
+Console.WriteLine("Ingrese el número de Identificación del estudiante:");
+string idstudents = Console.ReadLine();
+
+if (students.ContainsKey(idstudents))
+{
+    string studentname = students[idstudents];
+        Console.WriteLine($"La identificación {idstudents} corresponde a: {studentname}");
+    }
+    else
+    {
+        Console.WriteLine($"No se encontró ningún estudiante con la identificación {idstudents} en la colección.");
+    }
